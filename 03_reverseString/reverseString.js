aaaf8e78bcccefbd55539fa4ce8898b1a648cfb2
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-12 16:47:14 -0400

    add string reversing function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let counter = string.length - 1
+    let outputString = ""
 
+    for (let i = counter; i >=0; i--) {
+        outputString += string.charAt(i)
+    }
+
+    return outputString
 };
 
 // Do not edit below this line
