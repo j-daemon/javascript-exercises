0f8740937af45d837ab37a0f730bfbd205d720bb
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-11 21:17:31 -0400

    add ERROR for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    resultString = ""
 
+// return ERROR if negative
+
+    if (num < 0) {
+        resultString = "ERROR"
+        return resultString
+    }
+
+// for loop for all numbers above 0
+    else if (num >= 0) {
+        for (let i = num; i > 0; i--) {
+            resultString = resultString.concat(string)
+        };
+    
+        return resultString
+    };
+   
 };
 
 // Do not edit below this line
