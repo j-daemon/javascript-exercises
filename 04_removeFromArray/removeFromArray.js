663fd4fe761fa4cc3a18105d1b6c5916528b2633
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-12 21:18:02 -0400

    add indexing and push to output array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(userArray, ...args) {   
+    
+    resultantArray = []
+
+    for (let item of userArray) {
+        if (args.indexOf(item) >= 0) {
+            continue;
+        }
+        else {
+            resultantArray.push(item);
+        }
+    }   
+
+    return resultantArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
