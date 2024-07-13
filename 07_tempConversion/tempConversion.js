999e437b1abc8057e4ed619ca1eaf79fc9f90e17
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-13 11:39:24 -0400

    add conversion functions for C and T

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  
+  const conversionToCelsius = (tempF - 32) / 1.8
+  return Number(conversionToCelsius.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  
+  const conversionToFahrenheit = tempC * 1.8 + 32
+  return Number(conversionToFahrenheit.toFixed(1)) 
+
 };
 
 // Do not edit below this line
