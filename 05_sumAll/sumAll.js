c9c8205df20f5c9e0a3bc5b1dd843617181f31da
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-13 10:44:47 -0400

    add sumAll function with error checks

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+//Error checks for negative numbers and non-numbers
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+    else if (typeof (num1) !=="number" || typeof (num2) !=="number" ) {
+        return "ERROR"
+    }
+
+    else {
+
+//Identify the lowest and highest number
+
+    let startNum = Math.min(num1, num2)
+    let maxNum = Math.max(num1, num2)
+
+//create output variable set to 0
+
+    let outputNum = 0
+
+//Add each number from num1 to num2 to an output variable
+
+    for (let i = startNum; i <= maxNum; i++) {
+        outputNum += i
+    }
+
+//Return final output variable
+
+    return outputNum
+
+    }
 
 };
 
