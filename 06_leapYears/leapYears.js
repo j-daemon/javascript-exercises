25b17962fa519465ff7c2af18642705755ea62a3
j-deamon <82471286+j-daemon@users.noreply.github.com>
2024-07-13 11:08:21 -0400

    add leaYears function content

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(yearArg) {
+
+    if (yearArg % 4 == 0) {
+        if (yearArg % 100 == 0 && yearArg % 400 == 0) {
+            return true
+        }
+        else if (yearArg % 100 == 0) {
+            return false
+        }
+        
+        return true
+    }
+
+    else {
+        return false
+    }
+
 
 };
 
